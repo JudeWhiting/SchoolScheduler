da4f2e71c6e274f8e38631700974030bd387aedc
Jude <111361323+JudeWhiting@users.noreply.github.com>
2024-04-02 01:08:43 +0100

    Update classes.py

--- src/SchoolScheduler/classes.py
+++ src/SchoolScheduler/classes.py
@@ -1,28 +1,19 @@
-class Classes:
-    def __init__(self, ID, name):
+class Teacher:
+
+
+    def __init__(self, ID, name, subjects=[]):
+
         self.ID = ID
         self.name = name
-
-
-class Teachers:
-    def __init__(self, ID, name):
-        self.ID = ID
-        self.name = name
-
-class Rooms:
-    def __init__(self, ID, name):
-        
-
-class Courses(Classes, Teachers):
-    def __init__(self, courseID, classID, teacherID, subject_name, number_of_lessons):
-        self.courseID = courseID
-        self.classID = classID
-        self.teacherID = teacherID
-        self.subject_name = subject_name
-        self.number_of_lessons = number_of_lessons
-
+        self.subjects = subjects
 
 
 if __name__ == "__main__":
-    
 
+    # teacher0 = Teacher(0, 'A. Johnson', ['english'])
+    # print(Teacher.all_instances[0].name)
+
+    Teachers = []
+    Teachers.append(Teacher(len(Teachers), 'A. Johnson', ['eng', 'ict']))
+    Teachers.append(Teacher(len(Teachers), 'B. Bryson', ['math']))
+    print(Teachers[1].name)
