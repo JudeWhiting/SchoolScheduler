a8c26bf68274bd2ab2987a2cb8d6f4d872ceb53d
Jude <111361323+JudeWhiting@users.noreply.github.com>
2024-04-04 00:22:07 +0100

    work on generating the rooms next

--- src/SchoolScheduler/classes.py
+++ src/SchoolScheduler/classes.py
@@ -1,27 +1,43 @@
 import pandas as pd
+import numpy as np
 
 class Teacher:
 
-    def __init__(self, ID, name, subjects=[]):
+    teacher_count = 0
 
-        self.ID = ID
+    def __init__(self, name, subjects=[]):
+
+        self.ID = Teacher.teacher_count
         self.name = name
         self.subjects = subjects
 
-class Room:
-    def __init__(self, ID, name, block):
+        Teacher.teacher_count += 1
 
-        self.ID = ID
+class Room:
+
+    room_count = 0
+
+    def __init__(self, name, block):
+
+        self.ID = Room.room_count
         self.name = name
         self.block = block
 
+        Room.room_count += 1
+
 class Student:
 
-    def __init__(self, ID, name, year):
+    student_count = 0
 
-        self.ID = ID
+    def __init__(self, name, year):
+
+        self.ID = Student.student_count
         self.name = name
         self.year = year
+
+        Student.student_count += 1
+
+
 
 class Meeting:
 
@@ -29,19 +45,63 @@ class Meeting:
         pass
 
 
+Subjects = []
+Teachers = []
+Rooms = []
+Students = []
+
+with open ('src/data/teachers.txt', 'r') as file:
+    for line in file:
+        line = line.strip()
+        parts = line.split(' , ')
+        Teachers.append(Teacher(parts[0], parts[1:]))
+
+with open ('src/data/students.txt', 'r') as file:
+    for line in file:
+        line = line.strip()
+        parts = line.split(', ')
+        Students.append(Student(parts[0], parts[1]))
+
+with open ('src/data/rooms.txt') as file:
+    for line in file:
+        line = line.strip()
+        parts = line.split(' , ')
+        Rooms.append(Room())
 
 data = {
-    ('main', 'class'): ['c1','c2','c3','c4'],
-    ('monday', 'p1'): [0,0,0,0],
-    ('monday', 'p2'): [0,0,0,0],
-    ('monday', 'p3'): [0,0,0,0],
-    ('monday', 'p4'): [0,0,0,0],
-    ('monday', 'p5'): [0,0,0,0]
+    ('main', 'student'): Students
 }
 
 df = pd.DataFrame(data)
+for day in ['mon','tues','wed','thur','fri']:
+
+        for i in range(1,6):
+
+            df[(day, f'p{i}')] = np.zeros(len(Students))
+
 print(df)
 
+# Teachers = []
+# Teachers.append(Teacher(len(Teachers), 'A. Johnson', ['eng', 'ict']))
+# Teachers.append(Teacher(len(Teachers), 'B. Bryson', ['math']))
+
+# Rooms = []
+# Rooms.append(Room(len(Rooms), 'm1', 'math'))
+# Rooms.append(Room(len(Rooms), 'e1', 'eng'))
+# Rooms.append(Room(len(Rooms), 'c1', 'ict'))
+
+# Students = []
+# Students.append(Student('J. Whiting', 7))
+# Students.append(Student('B. Butten', 8))
+# Students.append(Student('H. Castley', 9))
+# Students.append(Student('H. Philippi', 10))
+
+
+
+
+
+# print(df[('main','student')][3].ID)
+# print()
 
 # # Create a DataFrame with a MultiIndex column
 # data = {
@@ -67,7 +127,3 @@ print(df)
 # print(df.sort_values(by='Age', ascending=False))
 
 
-# Teachers = []
-# Teachers.append(Teacher(len(Teachers), 'A. Johnson', ['eng', 'ict']))
-# Teachers.append(Teacher(len(Teachers), 'B. Bryson', ['math']))
-# print(Teachers[1].name)
